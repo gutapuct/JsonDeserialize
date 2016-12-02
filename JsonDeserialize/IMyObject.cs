04643f7570cbf113b242a0bed5bd2a4116ff05a7
Захар <aleshkin.z@flagmax.ru>
2016-12-02 22:55:06 +0300

    изменения

---
+++ JsonDeserialize/IMyObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace JsonDeserialize
+{
+    public interface IMyObject
+    {
+        void AddProperty(string name, object value = null);
+        Property GetPropertyByName(string name);
+        List<Property> GetProperties();
+    }
+}
--- JsonDeserialize/MyObject.cs
+++ JsonDeserialize/MyObject.cs
@@ -4,11 +4,11 @@ using System.Linq;
 
 namespace JsonDeserialize
 {
-    public class MyObject : IProperty
+    public class MyObject : IMyObject
     {
         private List<Property> _properties = new List<Property>();
 
-        public void SetProperty(string name, object value)
+        public void AddProperty(string name, object value = null)
         {
             if (_properties.Where(i => i.Name == name).Any())
             {
@@ -19,28 +19,12 @@ namespace JsonDeserialize
                 _properties.Add(new Property { Name = name, Value = value });
             }
         }
-        public void SetPropertyName(string name)
-        {
-            if (!_properties.Where(i => i.Name == name).Any())
-            {
-                _properties.Add(new Property { Name = name, Value = null });
-            }
-        }
-        public void SetPropertyValueByName(string name, object value)
-        {
-            if (_properties.Where(i => i.Name == name).FirstOrDefault() != null)
-            {
-                _properties.Where(i => i.Name == name).FirstOrDefault().Value = value;
-            }
-        }
+
         public Property GetPropertyByName(string name)
         {
             return _properties.Where(i => i.Name == name).FirstOrDefault();
         }
-        public List<Property> GetPropertiesByValue(object value)
-        {
-            return _properties.Where(i => i.Value == value).ToList();
-        }
+        
         public List<Property> GetProperties()
         {
             return _properties;
